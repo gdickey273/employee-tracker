fcb5e87b2e6665633214236c34dcf4fdc5c2c482
gdickey273 <gdickey273@gmail.com>
2020-09-16 13:00:27 -0400

    started coding inquirer functionality and function to prompt new employee information

--- employee-tracker.js
+++ employee-tracker.js
@@ -1,6 +1,7 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const cTable = require("console.table");
+const { createPromptModule } = require("inquirer");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -18,3 +19,60 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
+function start() {
+  inquirer.prompt({
+    name: "todo",
+    type: "list",
+    message: "What would you like to do?",
+    choices: ["Create Employee", "Create Role", "Create Department"]
+  }).then(answer => {
+    switch (answer.todo) {
+      case "Create Employee":
+        newEmployee();
+        break;
+
+      case "Create Role":
+        newRole();
+        break;
+
+      case "Create Department":
+        newDeparment();
+        break;
+
+
+    }
+  })
+}
+
+async function newEmployee() {
+  inquirer.prompt(
+    {
+      name:"first_name",
+      type: "input",
+      message: "What is the employee's first name?",
+      validate: function(value){
+      if(!/[^a-z-\s]/.test(value.toLowerCase())){
+        return true;
+      }
+      return false;
+    }
+    },
+    {
+      name:"last_name",
+      type: "input",
+      message: "What is the employee's last name?",
+      validate: function(value){
+        if(!/[^a-z-\s]/.test(value.toLowerCase())){
+          return true;
+        }
+        return false;
+      }
+    }
+
+  ).then(answers => {
+    console.log(answers.first_name);
+  })
+}
+
+
+start();
