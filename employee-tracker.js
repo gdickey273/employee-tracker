f3812d5ee7248a4dc624f0dfff058f4ba9a05d86
gdickey273 <gdickey273@gmail.com>
2020-09-17 11:24:03 -0400

    coded the inquirer prompt in newEmployee()

--- employee-tracker.js
+++ employee-tracker.js
@@ -31,11 +31,29 @@ function start() {
 
 async function newEmployee() {
   let employees = await query.getEmployees();
+  let employeeChoices = [];
+  for (let employee of employees){
+    employeeChoices.push(
+      {
+        name: `${employee.first_name} ${employee.last_name}`,
+        value: employee.id
+      });
+  }
+
   let roles = await query.getRoles();
-  console.log(employees);
-  console.log(roles);
+  let roleChoices = [];
+  for (let role of roles){
+    roleChoices.push(
+      {
+        name: role.title,
+        value: role.id
+      });
+  }
+
+  // console.log(employees);
+  // console.log(roles);
     inquirer.prompt(
-    {
+    [{
       name:"first_name",
       type: "input",
       message: "What is the employee's first name?",
@@ -61,11 +79,28 @@ async function newEmployee() {
       name: "role_id",
       type: "list",
       message: "What is the employee's role?",
-
-    }
-
+      choices: roleChoices
+    },
+    {
+      name:"hasManager",
+      type: "confirm",
+      message: "Does the employee have a manger?",
+      validate: function(value){
+        if(value.toLowerCase() === "y" || value.toLowerCase === "n"){
+          return true;
+        }
+        return false;
+      }
+    },
+    {
+      name:"manager_id",
+      type: "list",
+      message: "Who is the employee's manager?",
+      choices: employeeChoices,
+      when: (answ) => {return answ.hasManager;}
+    }]
   ).then(answers => {
-    console.log(answers.first_name);
+    console.log(console.table(answers));
   })
 }
 
